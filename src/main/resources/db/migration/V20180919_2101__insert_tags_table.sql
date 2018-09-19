e4ab5c75e44eb36b8a0727dc27c7815edad635fd
Yang Jie <jiyang@thoughtworks.com>
2018-09-19 21:39:51 +0800

    add all need table by migration

---
+++ src/main/resources/db/migration/V20180919_2101__insert_tags_table.sql
@@ -0,0 +1,3 @@
+insert into tags (name) values ("Meeting Review");
+insert into tags (name) values ("Learning DevOps");
+insert into tags (name) values ("Reading Books");
---
+++ src/main/resources/db/migration/V20180919_2105__insert_status_table.sql
@@ -0,0 +1,3 @@
+insert into status (name) values ("In progress");
+insert into status (name) values ("Blocked");
+insert into status (name) values ("To do");
